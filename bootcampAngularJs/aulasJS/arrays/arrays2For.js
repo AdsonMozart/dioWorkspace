f56de690d52a14a49a2c39ade2114c19cfa438f0
AdsonMozart <110190822+AdsonMozart@users.noreply.github.com>
2023-07-14 15:00:51 -0300

    AdsonMozartPoli
    adição do conteúdo de arrays e começo de importação

---
+++ bootcampAngularJs/aulasJS/arrays/arrays2For.js
@@ -0,0 +1,16 @@
+//Utiliza-se a estrutura de repetição "for" para percorrer uma quantidade específica de vezes
+
+const nome = 'Adson Mozart Santos Paixão';
+
+//Exibe a letra que está na posição dentro do parênteses
+console.log(nome[0]);
+
+//Percorre todas as letras até a quantidade máxima no nome inserido
+for (let i = 0; i < nome.length; i++) {
+    console.log(nome[i]);    
+}
+
+//Percorre uma lista até 9
+for (let i = 0; i < 10; i++) {
+    console.log(i);    
+}
---
+++ bootcampAngularJs/aulasJS/arrays/arrays3For2.js
@@ -0,0 +1,21 @@
+//Tirando a média das notas percorrendo pela estrutura de repetição for
+const notas = [];
+
+notas.push(10);
+notas.push(10);
+notas.push(8);
+notas.push(5);
+notas.push(8);
+notas.push(8);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+}
+
+const media = soma / notas.length;
+
+console.log(soma);
+console.log(media);
