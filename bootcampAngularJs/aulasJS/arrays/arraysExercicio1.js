a75e7f5808a22effb2aeb0b37d47c52086d7e3ee
AdsonMozart <mozar7.dev@gmail.com>
2023-07-14 11:56:23 -0300

    adicionando a conclusão de arrays e inicio de importação

---
+++ bootcampAngularJs/aulasJS/arrays/arraysExercicio1.js
@@ -0,0 +1,13 @@
+//1) Crie um programa que dado um número imprima a sua tabuada
+
+const numero = 5
+
+for (let i = 1; i <= 10; i++) {
+    console.log(numero * i);
+    
+}
+for (let i = 1; i <= 10; i++) {
+    console.log(numero +'x'+ i +' = '+ numero * i);
+    
+}
+
