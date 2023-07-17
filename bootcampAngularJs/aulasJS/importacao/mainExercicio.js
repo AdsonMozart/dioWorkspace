ba011f9c036fd0974035b08d6e2029addd71a530
AdsonMozart <mozar7.dev@gmail.com>
2023-07-17 11:17:06 -0300

    última modificação da parte de importação

---
+++ bootcampAngularJs/aulasJS/importacao/mainExercicio.js
@@ -0,0 +1,13 @@
+/* 1) Uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1 a 100.
+Faça um programa que receba os 5 números sorteados para os alunos e mostreo o maior número sorteador.
+
+Dados de entrada:
+5
+50
+10
+98
+23
+
+saída:
+98
+*/
